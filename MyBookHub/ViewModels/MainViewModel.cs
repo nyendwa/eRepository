926decb9503833ce01ee7287e95a0dff5fb9fb32
Schofield, stuart <546wine@gmail.com>
2023-03-21 17:05:50 -0600

    Added CommunityToolkit.MVVM Nuget package and Added MainViewModel to the ViewModels Folder

---
+++ MyBookHub/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBookHub.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+    }
+}
